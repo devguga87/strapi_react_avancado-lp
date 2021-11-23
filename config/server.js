b551ea39368ca6f5c1979dee7d451bf6dfa8bd85
Gustavo Seabra <devguga87@gmail.com>
2021-11-23 16:32:50 -0300

    feature:first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '5bced8cac620d141f0639144f3cc4ac7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2ee4d193-e374-4e7f-955b-ca49f5b6cb32'
+};
